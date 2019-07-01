e43405d134a1f40d49e3b476a1142a52b858b6f8
Jesus Lizama <dev@hellojeb.us>
2019-06-30 21:22:23 -0700

    update to run post

--- src/main.go
+++ src/main.go
@@ -9,12 +9,14 @@ import (
 
 type body struct {
 	 Message string `json="message"`
+	 Method string `json="method"`
+	 QueryParams map[string]string `json="query_params"`
 }
 
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	log.Println("Lambda Request", request.RequestContext.RequestID)
-	b, _ := json.Marshal(body{Message: "hello world"})
+	b, _ := json.Marshal(body{Message: "hello world", Method: request.HTTPMethod, QueryParams: request.QueryStringParameters})
 	return events.APIGatewayProxyResponse{
 		Body: string(b),
 		StatusCode: 200,
