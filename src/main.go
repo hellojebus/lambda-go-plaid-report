35b20ca3f69e4bef6c96ac467ad7daa970a78610
Jesus Lizama <dev@hellojeb.us>
2019-07-01 20:28:57 -0700

    add ssm support for keys

--- src/main.go
+++ src/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"github.com/aws/aws-lambda-go/events"
 	"log"
 	"encoding/json"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ssm"
+	"fmt"
 )
 
 type body struct {
@@ -15,6 +19,24 @@ type body struct {
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	log.Println("Lambda Request", request.RequestContext.RequestID)
+
+	sess, err := session.NewSessionWithOptions(session.Options{
+		Config:            aws.Config{Region: aws.String("us-west-1")},
+		SharedConfigState: session.SharedConfigEnable,
+	})
+	if err != nil {
+		panic(err)
+	}
+	ssmsvc := ssm.New(sess, aws.NewConfig().WithRegion("us-west-1"))
+	keyname := "/MyService/MyApp/Dev/DATABASE_URI"
+	withDecryption := false
+	param, err := ssmsvc.GetParameter(&ssm.GetParameterInput{
+		Name:           &keyname,
+		WithDecryption: &withDecryption,
+	})
+
+	value := *param.Parameter.Value
+	fmt.Println(value)
 	b, _ := json.Marshal(body{Message: "hello world", Method: request.HTTPMethod})
 	return events.APIGatewayProxyResponse{
 		Body: string(b),
